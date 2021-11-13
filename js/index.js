76b4bae2a9136270b1b87611fba7703593760ae1
mcampo09 <93607130+mcampo09@users.noreply.github.com>
2021-11-13 01:41:58 -0500

    mThe instructions were followed and the excercise is done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,76 @@
-// Iteration 1: Names and Input
-// 
+console.log("I'm ready!");
+let hacker1= "Juan";
+console.log (`The driver name is ${hacker1}`)
+let hacker2= "Cristopher";
+console.log(`The navigator's name is ${hacker2}`) 
 
-// Iteration 2: Conditionals
+if (hacker1.length > hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+}
+else if (hacker1.length > hacker2.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`);
+}
+else {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters`);
+}
+
+let DriversName = "";
+for(let i=0; i < hacker1.length; i++) {
+  DriversName += hacker1[i].toUpperCase() + " ";
+}
+
+let secondDriversName = "";
+for(let i=0; i < hacker2.length; i++) {
+  secondDriversName += hacker2[i].toUpperCase() + " ";
+}
+console.log(secondDriversName.trim());
+
+console.log(hacker1.split('').reverse().join(''));
+console.log(hacker2.split('').reverse().join(''));
 
 
-// Iteration 3: Loops
+hacker1 = hacker1.toLowerCase();
+hacker2 = hacker2.toLowerCase();
+
+if (hacker1 > hacker2) {
+  console.log("The driver's name goes first.")
+}
+else if (hacker1 < hacker2) {
+  console.log("Yo, the navigator goes first definitely.")
+}
+else {
+  console.log("What?! You both have the same name?")
+} 
+
+
+if (hacker1.localeCompare(hacker2) === -1) {
+  console.log("The driver's name goes first.")
+}
+else if (hacker1.localeCompare(hacker2) === 1) {
+  console.log("Yo, the navigator goes first definitely.")
+}
+else {
+  console.log("What?! You both have the same name?")
+} 
+const paragraph = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum rutrum nibh, ac efficitur augue consequat eu. Curabitur et luctus libero. Phasellus sit amet facilisis arcu. Sed auctor orci libero, in ultricies justo consequat vitae. Suspendisse vel lectus sit amet lacus aliquet laoreet. Vivamus justo elit, convallis quis accumsan facilisis, gravida vitae tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sed scelerisque ligula. Duis id ex sed diam convallis porttitor id sit amet magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis ut massa aliquet, auctor sapien nec, semper eros.
+
+Etiam condimentum rutrum arcu a sagittis. Quisque auctor mi sit amet nunc ornare fermentum. Mauris consequat ultrices volutpat. Donec nec finibus eros. Sed vel accumsan nulla. Nullam vitae elit commodo, mattis arcu id, tincidunt mi. Sed volutpat nulla a bibendum pretium.
+
+Suspendisse eleifend, quam in lobortis vestibulum, odio augue facilisis lectus, ut semper elit neque ac sapien. Maecenas a est placerat augue consectetur tincidunt. Praesent auctor, tortor non varius ultrices, risus magna gravida nulla, vitae suscipit purus turpis et elit. Nam mauris est, hendrerit vitae euismod sit amet, egestas sit amet justo. Vivamus dapibus a leo ac sollicitudin. Donec fringilla ac arcu eu molestie. Aliquam bibendum venenatis gravida. Integer dapibus placerat sapien vulputate auctor. Nulla tincidunt vestibulum elit. Ut at malesuada leo, sit amet fermentum est. Duis sit amet libero quis mi fringilla condimentum. Maecenas fringilla tellus ac enim finibus viverra. Proin a posuere ex.`
+
+let words = 0;
+let etCount = 0;
+
+let splittedParagraph = paragraph.split(' ');
+
+for (let i=0; i < splittedParagraph.length; i++) {
+  if(splittedParagraph[i].includes('\n')) {
+    words++
+  } else if(splittedParagraph[i] === "et" || splittedParagraph[i] === "et.") {
+    etCount++;
+  }
+  words++;
+ }
+
+console.log('words -->', words);
+console.log('et count  -->', etCount);
